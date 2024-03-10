b87ac2fbcdc3b47067df2690fb4c88ad5ec4478d
dsoFresherXuanHoa <dso.intern.xuanhoa@gmail.com>
2024-03-10 13:25:11 +0700

    feat: cli estimated finish time support

--- src/v1/cli/yargs.js
+++ src/v1/cli/yargs.js
@@ -6,8 +6,10 @@ const { hideBin } = require("yargs/helpers");
 const chalk = require("chalk");
 const axios = require("axios");
 const _ = require("lodash");
+const moment = require("moment");
 
-const { ON_RELEASE, MSG } = require("../../../constant");
+const { MSG } = require("../../../constant");
+const { CODE } = require("../api/rest/playlist/constant");
 
 const { DOMAIN_ADDRESS } = process.env;
 
@@ -56,17 +58,39 @@ yargs(hideBin(process.argv))
       const data = response.data;
       const [originIdsLength, uniqueIdsLength] = data.payload?.length;
       const duplicatedIds = data.payload?.duplicatedIds;
+      const [originItemsUnix, uniqueItemsUnix] = data.payload?.unix;
       const [originItemDurations, uniqueItemDurations, requireItemDurations] = data.payload?.duration;
       console.log(`Total: ${chalk.green(originIdsLength)} videos, with ${chalk.red(originIdsLength - uniqueIdsLength)} videos duplicated!`);
       if (!_.isEmpty(duplicatedIds)) {
         console.log(chalk.red("DUPLICATED VIDEO ID: "));
-        duplicatedIds.forEach((v, i, o) => console.log(chalk.strikethrough(`https://www.youtube.com/watch?v=${v}`)));
+        duplicatedIds.forEach((v, i, o) => console.log(chalk.red.strikethrough(`https://www.youtube.com/watch?v=${v}`)));
       }
-      console.log(`Total Length (Origin at 1.00X): ${originItemDurations}`);
-      console.log(`Total Length (Unique at 1.00X): ${chalk.yellow(uniqueItemDurations)}`);
-      console.log(`Total Length (Unique at ${speed.toFixed(2)}X): ${chalk.green(requireItemDurations)}`);
+      console.log(
+        `Total Length (Origin at 1.00X): ${originItemDurations} \nEstimated finish at: ${moment(Date.now() + originItemsUnix)
+          .local()
+          .format("YYYY-MM-DD HH:mm:ss")}`
+      );
+      console.log(
+        `Total Length (Unique at 1.00X): ${chalk.yellow(uniqueItemDurations)} \nEstimated finish at: ${chalk.yellow(
+          moment(Date.now() + uniqueItemsUnix)
+            .local()
+            .format("YYYY-MM-DD HH:mm:ss")
+        )}`
+      );
+      console.log(
+        `Total Length (Unique at ${speed.toFixed(2)}X): ${chalk.green(requireItemDurations)} \nEstimated finish at: ${chalk.green(
+          moment(Date.now() + uniqueItemsUnix / speed)
+            .local()
+            .format("YYYY-MM-DD HH:mm:ss")
+        )}`
+      );
     } catch (error) {
-      ON_RELEASE || console.error(chalk.red(`${MSG.CLI_EXECUTION_FAILURE}: ${error}`));
+      const errorCode = error.response?.data?.error?.code;
+      if (errorCode === CODE.PLAYLIST_NOT_FOUND) {
+        console.error(chalk.cyan("URL not in Youtube Playlist URL format! Try with another URL!"));
+      } else {
+        console.error(chalk.red(`${MSG.CLI_EXECUTION_FAILURE}: ${error}`));
+      }
     }
   })
   .demandCommand(1, "Need at least one command")
